7750c349ef9cd915d255d7652c6b234c9055d421
Kuzmich-Oleksandr <rodiona07ua@gmail.com>
2026-06-05 18:03:02 +0200

    Практична 12: реєстрація та авторизація ASP.NET Identity

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using JewelryStore.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System.Security.Claims;
+using Microsoft.Owin.Security;
+using Kuzmich_JewelryStore.Filters;
+
+namespace Kuzmich_JewelryStore.Controllers
+{
+    [Culture]
+    public class AccountController : Controller
+    {
+        private ApplicationUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext()
+                    .GetUserManager<ApplicationUserManager>();
+            }
+        }
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        // GET: Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Year = model.Year
+                };
+
+                IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        // GET: Login
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        // POST: Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = await UserManager.FindAsync(model.Email, model.Password);
+
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "Невірний логін або пароль.");
+                }
+                else
+                {
+                    ClaimsIdentity claim = await UserManager.CreateIdentityAsync(
+                        user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                    AuthenticationManager.SignOut();
+                    AuthenticationManager.SignIn(
+                        new AuthenticationProperties { IsPersistent = true }, claim);
+
+                    if (String.IsNullOrEmpty(returnUrl))
+                        return RedirectToAction("Index", "Home");
+                    return Redirect(returnUrl);
+                }
+            }
+            ViewBag.returnUrl = returnUrl;
+            return View(model);
+        }
+
+        // Logout
+        public ActionResult Logout()
+        {
+            AuthenticationManager.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Models/IdentityModels.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace JewelryStore.Models
+{
+    // Користувач
+    public class ApplicationUser : IdentityUser
+    {
+        public int Year { get; set; }
+    }
+
+    // Контекст Identity
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationContext() : base("IdentityDb") { }
+
+        public static ApplicationContext Create()
+        {
+            return new ApplicationContext();
+        }
+    }
+
+    // Менеджер користувачів
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store) { }
+
+        public static ApplicationUserManager Create(
+            IdentityFactoryOptions<ApplicationUserManager> options,
+            IOwinContext context)
+        {
+            ApplicationContext db = context.Get<ApplicationContext>();
+            ApplicationUserManager manager = new ApplicationUserManager(
+                new UserStore<ApplicationUser>(db));
+            return manager;
+        }
+    }
+
+    // Модель реєстрації
+    public class RegisterModel
+    {
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Email { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public int Year { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DataAnnotations.DataType(
+            System.ComponentModel.DataAnnotations.DataType.Password)]
+        public string Password { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DataAnnotations.Compare("Password",
+            ErrorMessage = "Паролі не співпадають")]
+        [System.ComponentModel.DataAnnotations.DataType(
+            System.ComponentModel.DataAnnotations.DataType.Password)]
+        public string PasswordConfirm { get; set; }
+    }
+
+    // Модель входу
+    public class LoginModel
+    {
+        [System.ComponentModel.DataAnnotations.Required]
+        public string Email { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        [System.ComponentModel.DataAnnotations.DataType(
+            System.ComponentModel.DataAnnotations.DataType.Password)]
+        public string Password { get; set; }
+    }
+}
