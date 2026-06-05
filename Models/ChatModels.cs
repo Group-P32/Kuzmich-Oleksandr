909015a898e0d1a1de11bf360b121ae738409b1d
Kuzmich-Oleksandr <rodiona07ua@gmail.com>
2026-06-05 17:44:36 +0200

    Практична 11: чат за допомогою AJAX та jQuery

---
+++ Models/ChatModels.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JewelryStore.Models
+{
+    public class ChatModel
+    {
+        public List<ChatUser> Users;
+        public List<ChatMessage> Messages;
+
+        public ChatModel()
+        {
+            Users = new List<ChatUser>();
+            Messages = new List<ChatMessage>();
+            Messages.Add(new ChatMessage()
+            {
+                Text = "Чат ювелірного магазину розпочато " + DateTime.Now
+            });
+        }
+    }
+
+    public class ChatUser
+    {
+        public string Name;
+        public DateTime LoginTime;
+        public DateTime LastPing;
+    }
+
+    public class ChatMessage
+    {
+        public ChatUser User;
+        public DateTime Date = DateTime.Now;
+        public string Text = "";
+    }
+}
